11bacf76b4ec3758595c29ae537adcbf32de94c1
sketchbook21 <gilbert.hsu2@gmail.com>
2019-11-17 21:30:08 -0500

    cleanup

--- app/javascript/react/containers/HomeContainer.js
+++ app/javascript/react/containers/HomeContainer.js
@@ -5,48 +5,6 @@ import Chart from "../tiles/Chart";
 import Ticker from "../tiles/Ticker";
 import DailyHistoryContainer from "./DailyHistoryContainer";
 
-const fetchSingleSymbol = async symbol => {
-  const key = "2NSG0O0E1I8ESDEZ";
-
-  try {
-    const response = await axios.get(
-      `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=${key}`
-    );
-
-    return response["data"]["Global Quote"];
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-const getAllSymbolData = async symbolList => {
-  const dataArray = [];
-  const stock1 = await fetchSingleSymbol(symbolList[0]);
-  dataArray.push(stock1);
-  const stock2 = await fetchSingleSymbol(symbolList[1]);
-  dataArray.push(stock2);
-  const stock3 = await fetchSingleSymbol(symbolList[2]);
-  dataArray.push(stock3);
-  const stock4 = await fetchSingleSymbol(symbolList[3]);
-  dataArray.push(stock4);
-  const stock5 = await fetchSingleSymbol(symbolList[4]);
-  dataArray.push(stock5);
-  // const stock6 = await fetchSingleSymbol(symbolList[5]);
-  // dataArray.push(stock6);
-  // const stock7 = await fetchSingleSymbol(symbolList[6]);
-  // dataArray.push(stock7);
-  // const stock8 = await fetchSingleSymbol(symbolList[7]);
-  // dataArray.push(stock8);
-  // const stock9 = await fetchSingleSymbol(symbolList[8]);
-  // dataArray.push(stock9);
-  // const stock10 = await fetchSingleSymbol(symbolList[9]);
-  // dataArray.push(stock10);
-  // const stock11 = await fetchSingleSymbol(symbolList[10]);
-  // dataArray.push(stock11);
-
-  return await dataArray;
-};
-
 class HomeContainer extends Component {
   constructor(props) {
     super(props);
@@ -68,7 +26,6 @@ class HomeContainer extends Component {
       sp500: {},
       sp10: {}
     };
-    this.fetchData = this.fetchData.bind(this);
   }
 
   componentDidMount() {
@@ -85,48 +42,6 @@ class HomeContainer extends Component {
 
   buttonClick = async () => {
     this.fetchData(this.state.symbolList);
-
-    // getAllSymbolData(this.state.symbolList.slice(0, 5)).then(data => {
-    //   console.log(data);
-    //   this.setState({ fetchedData: data });
-    // });
-
-    // console.log("start 60 seconds");
-    // setTimeout(function() {
-    //   console.log("waited 60 seconds");
-    // }, 60000);
-
-    // getAllSymbolData(this.state.symbolList.slice(0, 5)).then(data => {
-    //   console.log(data);
-    //   this.setState({ fetchedData: data });
-    // });
-
-    // const a = await setTimeout(() => {
-    //   getAllSymbolData(this.state.symbolList).then(data => {
-    //     console.log(data);
-    //     this.setState({ symbolData: data });
-    //   });
-    // }, 5000);
-
-    // console.log(a);
-
-    // const b = await setTimeout(() => {
-    //   getAllSymbolData(this.state.symbolList).then(data => {
-    //     console.log(data);
-    //     this.setState({ symbolData: data });
-    //   });
-    // }, 5000);
-
-    // console.log(b);
-
-    // console.log(this.state.symbolData);
-  };
-
-  testClick = () => {
-    const cheese = this.state.symbolList.map(symbol => {
-      return `${symbol} is great`;
-    });
-    console.log(cheese);
   };
 
   fetchData = symbolList => {
