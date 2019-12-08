c02adb60300bfb08c0614e5e36ebbafc3ea4e98c
sketchbook21 <gilbert.hsu2@gmail.com>
2019-12-08 17:19:46 -0500

    hook up and format heatmap

--- app/javascript/react/tiles/Heatmap.js
+++ app/javascript/react/tiles/Heatmap.js
@@ -5,13 +5,14 @@ import ReactTooltip from "react-tooltip";
 export const Heatmap = ({ records }) => {
   const chronologicalRecords = records && records.reverse();
 
-  console.log(records);
-
+  // format records for heatmap data needs
   const chartValues = chronologicalRecords.map(record => {
     let dataRow = {};
     dataRow["date"] = offsetDate(record["date_raw"], 1);
     dataRow["date_string"] = record["date"];
-    dataRow["delta"] = record["delta"];
+    const formatDelta =
+      record["delta"] > 0 ? `+${record["delta"]}%` : `${record["delta"]}%`;
+    dataRow["delta"] = formatDelta;
     let color = 8;
 
     if (record["delta"] < 0) {
@@ -41,17 +42,11 @@ export const Heatmap = ({ records }) => {
     return dataRow;
   });
 
-  const windowSize = window.addEventListener("resize", function() {
-    return window.innerWidth;
-  });
-
-  console.log(window.innerWidth);
+  const mobileScroll =
+    window.innerWidth < 1024 ? "scrolling-wrapper-mobile" : "";
 
   const startDate = new Date(chartValues[0].date);
   const endDate = new Date(chartValues[chartValues.length - 1].date);
-
-  const mobileScroll =
-    window.innerWidth < 1024 ? "scrolling-wrapper-mobile" : "";
 
   return (
     <div>
@@ -69,9 +64,13 @@ export const Heatmap = ({ records }) => {
           }}
           values={chartValues}
           tooltipDataAttrs={value => {
-            return {
-              "data-tip": `${value.date_string} has count: ${value.delta}`
-            };
+            if (value.date_string) {
+              return {
+                "data-tip": `${value.date_string} - SP10 / S&P 500 Delta: ${value.delta}`
+              };
+            } else {
+              return { "data-tip": "No data available for this day." };
+            }
           }}
         />
         <ReactTooltip />
