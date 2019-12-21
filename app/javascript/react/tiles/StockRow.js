0c3ea2f7572503c217ab523cde9a38a654c8eff9
sketchbook21 <gilbert.hsu2@gmail.com>
2019-12-20 22:53:45 -0500

    style stocks

--- app/javascript/react/tiles/StockRow.js
+++ app/javascript/react/tiles/StockRow.js
@@ -1,23 +1,19 @@
 import React from "react";
 
-export const StockRow = props => {
-  const name = props.stockData["name"];
+export const StockRow = ({ stockData }) => {
+  const name = stockData["name"];
 
-  const price = `$${props.stockData["price"].toFixed(2)}`;
+  const price = `$${stockData["price"].toFixed(2)}`;
 
   const percentChange =
-    props.stockData["change_percent"] >= 0 ? (
-      <span className="green">
-        +{props.stockData["change_percent"].toFixed(2)}%
-      </span>
+    stockData["change_percent"] >= 0 ? (
+      <span className="green">+{stockData["change_percent"].toFixed(2)}%</span>
     ) : (
-      <span className="red">
-        {props.stockData["change_percent"].toFixed(2)}%
-      </span>
+      <span className="red">{stockData["change_percent"].toFixed(2)}%</span>
     );
 
   return (
-    <div className="row">
+    <div className="row stock-row">
       <div className="small-4 columns">{name}</div>
       <div className="small-4 columns text-right">{price}</div>
       <div className="small-4 columns text-right">{percentChange}</div>
