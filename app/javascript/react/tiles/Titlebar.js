8e9a1b52216ad9a1d6d472ddfa7adf7c4202dfca
sketchbook21 <gilbert.hsu2@gmail.com>
2019-12-23 22:31:37 -0500

    style modal

--- app/javascript/react/tiles/Titlebar.js
+++ app/javascript/react/tiles/Titlebar.js
@@ -12,21 +12,45 @@ export const Titlebar = () => {
     setModalIsOpen(false);
   };
 
-  const customStyles = {
+  let modalStyle = {
     content: {
-      top: "50%",
+      top: "35%",
       left: "50%",
       right: "auto",
       bottom: "auto",
       marginRight: "-50%",
       transform: "translate(-50%, -50%)",
-      width: "75%",
-      padding: "35px"
+      width: "50%",
+      padding: "50px"
     },
     overlay: {
       background: "rgba(0, 0, 0, 0.5)"
     }
   };
+
+  const mobileFormat = x => {
+    if (x.matches) {
+      modalStyle = {
+        content: {
+          top: "50%",
+          left: "50%",
+          right: "auto",
+          bottom: "auto",
+          marginRight: "-50%",
+          transform: "translate(-50%, -50%)",
+          width: "75%",
+          padding: "20px"
+        },
+        overlay: {
+          background: "rgba(0, 0, 0, 0.5)"
+        }
+      };
+    }
+  };
+
+  const x = window.matchMedia("(max-width: 600px)");
+  mobileFormat(x); // Call listener function at run time
+  x.addListener(mobileFormat); // Attach listener function on state changes
 
   return (
     <div className="row container">
@@ -42,7 +66,7 @@ export const Titlebar = () => {
           <Modal
             isOpen={modalIsOpen}
             onRequestClose={closeModal}
-            style={customStyles}
+            style={modalStyle}
             contentLabel="Example Modal"
             ariaHideApp={false}
           >
@@ -54,7 +78,7 @@ export const Titlebar = () => {
               constituents of the S&P 500. These 10 stocks disproportionately
               combine to represent ~22.5% of the US market index.
             </div>
-            <div className="horizontal-spacer">
+            <div>
               Therefore, if you believe that the US market will grow in the
               long-term, the highest-weighted stocks will be disproportionately
               responsible for this growth.
